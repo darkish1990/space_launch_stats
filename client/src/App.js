bf59d16219c57c35356773f108ca7166353d89da
Roey Liba <rliba1990@gmail.com>
2020-03-08 14:22:04 +0200

    add client

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./App.css";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Launches from "./components/Launches";
+import Launch from "./components/Launch";
+
+const client = new ApolloClient({
+  uri: "/graphql"
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="container">
+          <h1 style={{ width: 300, display: "block", margin: "auto" }}>
+            SpaceX
+          </h1>
+
+          <Route exact path="/" component={Launches} />
+          <Route exact path="/launch/:flight_number" component={Launch} />
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
